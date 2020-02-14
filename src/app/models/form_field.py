804c04f647745ed08242d108a1d996b4d7562730
Teemmer <dziga2000@gmail.com>
2020-02-14 18:49:22 +0200

    Added fields in Form model

--- src/app/models/form_field.py
+++ src/app/models/form_field.py
@@ -11,7 +11,10 @@ class FormField(AbstractModel):
         DB.UniqueConstraint('form_id', 'position', name='unique_form_position'),
     )
 
-    form_id = DB.Column(DB.Integer, DB.ForeignKey('form.id'), nullable=False)
-    field_id = DB.Column(DB.Integer, DB.ForeignKey('field.id'), nullable=False)
+    form_id = DB.Column(DB.Integer, DB.ForeignKey('forms.id'), nullable=False)
+    field_id = DB.Column(DB.Integer, DB.ForeignKey('fields.id'), nullable=False)
     question = DB.Column(DB.Text, nullable=False)
     position = DB.Column(DB.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'form_id: {self.form_id}; field_id: {self.field_id}'
