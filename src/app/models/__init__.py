a94f018e25d0ccc95d1573cc7b644a5db9303966
Ivan Kostiv <45687472+IvanKostiv@users.noreply.github.com>
2020-02-17 17:16:28 +0200

    Feature/#14 (#51)
    * Add SettingsAutocomplete model
    
    * Add model to __init__.py
    
    * Added repr to model
    
    * Fixed pylint
    
    * Fixed docstring

--- src/app/models/__init__.py
+++ src/app/models/__init__.py
@@ -7,5 +7,6 @@ from .form import Form
 from .field import Field
 from .choice_option import ChoiceOption
 from .form_field import FormField
+from .setting_autocomplete import SettingAutocomplete
 from .shared_field import SharedField
 from .range import Range
