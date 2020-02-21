8a15b062d338a15ac97bae74a8f915732936c2fb
Yurii <yurdosii.ksv@gmail.com>
2020-02-21 16:05:39 +0200

    Changed Field model

--- src/app/models/field.py
+++ src/app/models/field.py
@@ -29,4 +29,3 @@ class Field(AbstractModel):
     shared_fields = DB.relationship('SharedField', backref='field')
     settings_autocomplete = DB.relationship('SettingAutocomplete', backref='field')
     fields_range = DB.relationship('FieldRange', backref='field')
-    settings_strict = DB.relationship('SettingStrict', backref='field')
