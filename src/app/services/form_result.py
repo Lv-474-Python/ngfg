56d2bc0df32aa667df7ada12685a7363df14a7a0
IvanKostiv <ivankostiv.pz@gmail.com>
2020-02-21 18:27:58 +0200

    Update FormResult model

--- src/app/services/form_result.py
+++ src/app/services/form_result.py
@@ -14,20 +14,20 @@ class FormResultService:
 
     @staticmethod
     @transaction_decorator
-    def create(user_id, form_id, answer):
+    def create(user_id, form_id, answers):
         """
         Create FormResult model
 
         :param user_id:
         :param form_id:
-        :param answer:
+        :param answers:
         :return: FormResult object or None
         """
 
         form_result = FormResult(
             user_id=user_id,
             form_id=form_id,
-            answer=answer
+            answers=answers
         )
 
         DB.session.add(form_result)
@@ -46,14 +46,15 @@ class FormResultService:
         return setting_autocomplete
 
     @staticmethod
-    def filter(form_result_id=None, user_id=None, form_id=None, answer=None):
+    def filter(form_result_id=None, user_id=None, form_id=None, answers=None, created=None):
         """
         FormResult filter method
 
         :param form_result_id:
         :param user_id:
         :param form_id:
-        :param answer:
+        :param answers:
+        :param created:
         :return: list of FormResult objects or empty list
         """
         filter_data = {}
@@ -64,8 +65,10 @@ class FormResultService:
             filter_data['user_id'] = user_id
         if form_id is not None:
             filter_data['form_id'] = form_id
-        if answer is not None:
-            filter_data['answer'] = answer
+        if answers is not None:
+            filter_data['answers'] = answers
+        if created is not None:
+            filter_data['created'] = created
 
         result = FormResult.query.filter_by(**filter_data).all()
         return result
