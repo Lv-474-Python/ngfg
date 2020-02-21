6bd8570d5d47ce9d9af5728be626b1dd5fd7bb13
IvanKostiv <ivankostiv.pz@gmail.com>
2020-02-21 11:29:27 +0200

    Fix filter method

--- src/app/services/setting_autocomplete.py
+++ src/app/services/setting_autocomplete.py
@@ -1,13 +1,28 @@
+"""
+SettingAutocomplete service
+"""
+
 from app.models import SettingAutocomplete
 from app import DB
 from app.helper.decorators import transaction_decorator
 
 
 class SettingAutocompleteService:
-
+    """
+    Class for SettingAutocomplete model
+    """
     @staticmethod
     @transaction_decorator
     def create(data_url, sheet, from_row, to_row, field_id):
+        """
+        Create SettingAutocomplete record in DB
+        :param data_url:
+        :param sheet:
+        :param from_row:
+        :param to_row:
+        :param field_id:
+        :return: setting_autocomplete or None
+        """
         setting_autocomplete = SettingAutocomplete(
             data_url=data_url,
             sheet=sheet,
@@ -20,7 +35,12 @@ class SettingAutocompleteService:
 
     @staticmethod
     @transaction_decorator
-    def update(setting_autocomplete_id, data_url=None, sheet=None, from_row=None, to_row=None, field_id=None):
+    def update(setting_autocomplete_id,  # pylint: disable=too-many-arguments
+               data_url=None,
+               sheet=None,
+               from_row=None,
+               to_row=None,
+               field_id=None):
 
         setting_autocomplete = SettingAutocomplete.query.get(setting_autocomplete_id)
 
@@ -42,16 +62,39 @@ class SettingAutocompleteService:
     @staticmethod
     @transaction_decorator
     def delete(setting_autocomplete_id):
-        setting_autocomplete = SettingAutocomplete.query.get(setting_autocomplete_id)
+        # TODO raise error
+        setting_autocomplete = SettingAutocompleteService.get_by_id(setting_autocomplete_id)
         DB.session.delete(setting_autocomplete)
 
         return True
 
     @staticmethod
     def get_by_id(setting_autocomplete_id):
-        setting_autocomplete = SettingAutocomplete.query.get(setting_autocomplete_id)
+        setting_autocomplete = SettingAutocompleteService.get_by_id(setting_autocomplete_id)
         return setting_autocomplete
 
     @staticmethod
-    def filter(**kwargs):
-        return SettingAutocomplete.query.filter_by(**kwargs).all()
+    def filter(setting_autocomplete_id=None,
+               data_url=None,
+               sheet=None,
+               from_row=None,
+               to_row=None,
+               field_id=None):
+
+        filter_data = {}
+
+        if setting_autocomplete_id is not None:
+            filter_data['id'] = setting_autocomplete_id
+        if data_url is not None:
+            filter_data['data_url'] = data_url
+        if sheet is not None:
+            filter_data['sheet'] = sheet
+        if from_row is not None:
+            filter_data['from_row'] = from_row
+        if to_row is not None:
+            filter_data['to_row'] = to_row
+        if field_id is not None:
+            filter_data['field_id'] = field_id
+
+        result = SettingAutocomplete.query.filter_by(**filter_data).all()
+        return result
