3494d90e2c808abbe1b6f730bfd0bd9e440d258b
AndriiKret <andrii.kret@gmail.com>
2020-02-22 10:15:54 +0200

    Added is_active with get and set, and minimal changes

--- src/app/models/user.py
+++ src/app/models/user.py
@@ -14,9 +14,10 @@ class User(AbstractModel, UserMixin):
 
     __tablename__ = 'users'
 
-    username = DB.Column(DB.String, nullable=False)
+    username = DB.Column(DB.String, nullable=True)
     email = DB.Column(DB.String, unique=True, nullable=False)
-    google_token = DB.Column(DB.Text, unique=True, nullable=False)
+    google_token = DB.Column(DB.Text, unique=True, nullable=True)
+    _is_active = DB.Column(DB.Boolean, nullable=True, default=False)
 
     forms = DB.relationship('Form', backref='owner')
     fields = DB.relationship('Field', backref='owner')
@@ -24,3 +25,11 @@ class User(AbstractModel, UserMixin):
     form_results = DB.relationship('FormResult', backref='user')
     groups = DB.relationship('Group', backref='user')
     groups_users = DB.relationship('GroupUser', backref='user')
+
+    @property
+    def is_active(self):
+        return self._is_active
+
+    @is_active.setter
+    def is_active(self, value):
+        self._is_active = value
