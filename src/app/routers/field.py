6020b538881366beca2fe4d5673875dbcca3dc4e
AndriiKret <andrii.kret@gmail.com>
2020-02-23 19:27:47 +0200

    Field prototype

--- src/app/routers/field.py
+++ src/app/routers/field.py
@@ -7,29 +7,48 @@ from app.models import Field
 name_space = API.namespace('FIELD_API', description='NgFg APIs')
 ns_forms = API.namespace('fields', description='NgFg APIs')
 
-model = API.model('fields', {
-    'name': fields.String(required=True, description="Name of the field", help="Name cannot be blank."),
-    'owner_id': fields.Integer(required=False, description="ID of owner", help="Name cannot be blank."),
-    'field_type': fields.Integer(required=True, description="Type of field", help="Name cannot be blank.")
+# main_model = API.model('fields', {
+#     'name': fields.String(required=True, description="Name of the field", help="Name cannot be blank."),
+#     'owner_id': fields.Integer(required=False, description="ID of owner", help="Name cannot be blank."),
+#     'field_type': fields.Integer(required=True, description="Type of field", help="Name cannot be blank.")
+# })
+main_model = API.model('fields', {
+    'name': fields.String,
+    'owner_id': fields.Integer,
+    'field_type': fields.Integer
+})
+autocomplete_model = API.model('setting_autocomplete',{
+        "data_url": fields.String,
+        "sheet": fields.String,
+        "from_row": fields.String,
+        "to_row": fields.String,
+    })
+
+extended_model = API.inherit('extended_field', main_model, {
+    "range_max": fields.Integer,
+    "range_min": fields.Integer,
+    "choice_options": fields.List(fields.String),
+    "setting_autocomplete": fields.Nested(autocomplete_model)
 })
 
 
 @name_space.route("/")
-class FieldCRUD(Resource):
-    @API.expect(model)
+class FieldAPI(Resource):
+    @API.expect(extended_model)
     def post(self):
         try:
-            name = request.json['name']
-            owner_id = request.json['owner_id']
-            field_type = request.json['field_type']
-            print(request.json)
-
-
-            return {
-                "name": name,
-                "owner_id": owner_id,
-                "field_type": field_type
-            }
+            # name = request.json['name']
+            # owner_id = request.json['owner_id']
+            # field_type = request.json['field_type']
+            req = request.json
+            for i in req:
+                print(req[f'{i}'])
+            # return {
+            #     "name": name,
+            #     "owner_id": owner_id,
+            #     "field_type": field_type
+            # }
+            return {'succ': 'kono Dio da'}
         except KeyError as e:
             name_space.abort(500, e.__doc__,
                              status="Could not save information",
--- src/app/services/field_post.py
+++ src/app/services/field_post.py
@@ -8,26 +8,30 @@ class FieldPost:
     @staticmethod
     @transaction_decorator
     def create(name, owner_id, is_strict, field_type, **kwargs):
-        # Number = 1
-        # Text = 2
-        # TextArea = 3
-        # Radio = 4
-        # Autocomplete = 5
-        # Checkbox = 6
-        a = {
-            "name": "string",
-            "owner_id": 0,
-            "is_strict": False,
-            "field_type": 1,  # number, string
-            "range": {
-                "min": 1,
-                "max": 10}
-        }
+        Number = 1
+        Text = 2
+        TextArea = 3
+        Radio = 4
+        Autocomplete = 5
+        Checkbox = 6
+
         field_instance = FieldService.create(name=name, owner_id=owner_id, field_type=field_type, is_strict=is_strict)
 
-        if field_type == FieldType.Number or FieldType.Text:
+        if field_type == Number or field_type == Text:
             range_min = kwargs.get('range_min', 0)
             range_max = kwargs.get('range_max', 255)
             range_instance = RangeService.create(range_min, range_max)
             FieldRangeService.create(field_instance.id, range_instance.id)
 
+        elif field_type == Radio or field_type == Checkbox:
+            choice_options = kwargs.get('choice_options')
+            print(choice_options)
+            for option in choice_options:
+                ChoiceOptionService.create(field_instance.id, option)
+
+        elif field_type == Autocomplete:
+            data_url = kwargs.get('data_url')
+            sheet = kwargs.get('sheet')
+            from_row = kwargs.get('from_row')
+            to_row = kwargs.get('to_row')
+            SettingAutocompleteService.create(data_url, sheet, from_row, to_row, field_instance)
