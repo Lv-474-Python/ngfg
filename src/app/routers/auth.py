0b57dfbf4b3e755b59f3a0c63e32d394ea49dcca
IvanKostiv <ivankostiv.pz@gmail.com>
2020-02-25 18:41:35 +0200

    Fixed auth

--- src/app/routers/auth.py
+++ src/app/routers/auth.py
@@ -4,19 +4,46 @@ app to auth user by google services
 import os
 import requests
 
-from flask import redirect, url_for
+from flask import redirect, url_for, jsonify
 from flask_login import (
     current_user,
     login_required,
     login_user,
     logout_user
 )
-
-from app import APP, GOOGLE_CLIENT
+from flask_restx import Resource, fields
+from werkzeug.exceptions import NotFound
+from app import APP, GOOGLE_CLIENT, API
 from app.config import GOOGLE_PROVIDER_CONFIG
 from app.services import UserService
 
 APP.secret_key = os.environ.get("APP_SECRET_KEY")
+
+AUTH_NS = API.namespace('auth', 'Auth APIs')
+MODEL = API.model('User', {
+    'google_token': fields.String(required=True, description="User google token")
+})
+
+
+@AUTH_NS.route('/login/')
+class LoginAPI(Resource):
+    def get(self):
+        if not current_user.is_authenticated:
+            return GOOGLE_CLIENT.authorize(
+                callback=url_for('callback', _external=True)
+            )
+        return redirect("/", code=302)
+
+
+@AUTH_NS.route('/logout/')
+class LogoutAPI(Resource):
+    # @login_required
+    def get(self):
+        logout_user()
+        return jsonify({
+            'code': 200,
+            'message': 'Unauthorized'
+        })
 
 
 @APP.route('/home_page/')
@@ -29,10 +56,10 @@ def index():
     if current_user.is_authenticated:
         return (
             f"Hello, {current_user.username} <br>"
-            "<a class='button' href='/logout'>Logout</a>"
+            "<a class='button' href='/api/v1/auth/logout/'>Logout</a>"
         )
 
-    return '<a class="button" href="/login">Google Login</a>'
+    return '<a class="button" href="/api/v1/auth/login/">Google Login</a>'
 
 
 @APP.route('/login')
@@ -49,7 +76,7 @@ def login():
     return redirect(url_for('index'))
 
 
-@APP.route('/login/callback')
+@APP.route('/api/v1/auth/login/callback')
 @GOOGLE_CLIENT.authorized_handler
 def callback(response):
     """
@@ -79,16 +106,19 @@ def callback(response):
         UserService.activate_user(user.id)
         login_user(user)
 
-    return redirect(url_for('index'))
+    return jsonify(
+        code=200
+    )
 
 
 @APP.route('/logout/')
-@login_required
+# @login_required
 def logout():
     """
     View for logout
 
     :return:
-    """
-    logout_user()
+        """
+    print(request.cookies.get('siss'))
+    # logout_user()
     return redirect(url_for('index'))
