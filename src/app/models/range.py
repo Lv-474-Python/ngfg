2495e5c1954bc8f5408ac9c4c30182d89fc7666a
AndriiKret <andrii.kret@gmail.com>
2020-02-27 14:25:13 +0200

    Added range min and max validator

--- src/app/models/range.py
+++ src/app/models/range.py
@@ -1,8 +1,9 @@
 """Range model"""
 
-from marshmallow import fields
+from marshmallow import fields, validates_schema, ValidationError
 from app import DB, MA
 from .abstract_model import AbstractModel
+from app.helper.constants import MIN_POSTGRES_INT, MAX_POSTGRES_INT
 
 
 class Range(AbstractModel):
@@ -41,3 +42,13 @@ class RangeSchema(MA.Schema):
 
     min = fields.Integer(required=False)
     max = fields.Integer(required=False)
+
+    @validates_schema
+    def validate_range(self, data, **kwargs):
+        min_value, max_value = data.get('min'), data.get('max')
+        if min_value and not MIN_POSTGRES_INT < min_value < MAX_POSTGRES_INT:
+            raise ValidationError(f'min must be between {MIN_POSTGRES_INT} and {MAX_POSTGRES_INT}')
+        if max_value and not MIN_POSTGRES_INT < max_value < MAX_POSTGRES_INT:
+            raise ValidationError(f'max must be between {MIN_POSTGRES_INT} and {MAX_POSTGRES_INT}')
+        if (min_value and max_value) and min_value > max_value:
+            raise ValidationError('min must be less than max')
