023adb0a6474ff412659a4beb532d07e806d83b4
AndriiKret <andrii.kret@gmail.com>
2020-03-12 18:14:02 +0200

    Added rows_autocomplete validators

--- src/app/schemas/setting_autocomplete.py
+++ src/app/schemas/setting_autocomplete.py
@@ -1,9 +1,10 @@
 """
 SettingAutocomplete schemas
 """
-from marshmallow import fields
+from marshmallow import fields, validates_schema, ValidationError
 
 from app import MA
+from urllib.parse import urlparse
 
 
 class SettingAutocompleteSchema(MA.Schema):
@@ -15,9 +16,24 @@ class SettingAutocompleteSchema(MA.Schema):
         """
         Setting autocomplete schema meta
         """
-        fields = ("dataUrl", "sheet", "fromRow", "toRow")
+        fields = ("data_url", "sheet", "from_row", "to_row")
 
     data_url = fields.Url(required=True, data_key="dataUrl")
     sheet = fields.Str(required=True)
     from_row = fields.Str(required=True, data_key="fromRow")
     to_row = fields.Str(required=True, data_key="toRow")
+
+    @validates_schema
+    def validate_data_url(self, data, **kwargs):
+        parsed_url = urlparse(data.get('data_url'))
+        if parsed_url.netloc != 'docs.google.com':
+            raise ValidationError('You entered wrong url')
+
+    @validates_schema
+    def validate_rows(self, data, **kwargs):
+        from_row = data.get('from_row')
+        to_row = data.get('to_row')
+        if not from_row.isalnum() or not to_row.isalnum():
+            raise ValidationError('Wrong entered row limits')
+        if from_row.isnumeric() or to_row.isnumeric():
+            raise ValidationError('Entered only row/rows without column')
