a87c7666925f699d06fb28277de7b7112bf0119a
IvanKostiv <ivankostiv.pz@gmail.com>
2020-03-17 16:45:59 +0200

    Added created field to Form, Field and Group model

--- src/app/models/form.py
+++ src/app/models/form.py
@@ -2,6 +2,8 @@
 Form database model.
 Table for storing descriptive information about the form.
 """
+from sqlalchemy import func
+
 from app import DB
 from .abstract_model import AbstractModel
 
@@ -27,6 +29,7 @@ class Form(AbstractModel):
     title = DB.Column(DB.String, nullable=False)
     result_url = DB.Column(DB.Text)
     is_published = DB.Column(DB.Boolean, nullable=False)
+    created = DB.Column(DB.TIMESTAMP(timezone=True), server_default=func.now(), nullable=False)
 
     fields = DB.relationship('FormField', backref='form', cascade='all,delete')
     form_results = DB.relationship('FormResult', backref='form')
