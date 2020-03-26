ac26830d8f1c2e8fd19e7b449a66a6d04e50fc2d
Yurii <yurdosii.ksv@gmail.com>
2020-03-26 17:33:02 +0200

    Added SheetManager tests

--- src/tests/app/helper/test_sheet_manager.py
+++ src/tests/app/helper/test_sheet_manager.py
@@ -4,10 +4,189 @@ Test SheetManager
 
 import mock
 import pytest
-# from pytest_mock import mocker
+import googleapiclient
 
-# from tests import SheetManager
 from app.helper.sheet_manager import SheetManager
+
+
+TEST_GET_DATA_WITH_RANGE_TRUE_DATA = [
+    (("1Vg2q1qeksamWE9", 'A1', 'A13'), ['test1.1', 'test1.2']),
+    (("1Vg1yuasdasddsa", 'A100', 'A130'), ['test2.1', 'test2.2']),
+]
+@pytest.mark.parametrize(
+    "test_input, expected", 
+    TEST_GET_DATA_WITH_RANGE_TRUE_DATA
+)
+def test_get_data_with_range_true(test_input, expected):
+    """
+    Test SheetManager get_data_with_range()
+    Test case when method executed successfully
+
+    :param test_input: test input data (spreadsheet_id, from_row, to_row)
+    :param expected: test expected result
+    """
+    mock_patch_path = 'app.helper.sheet_manager.SheetManager.service.spreadsheets'
+    with mock.patch(mock_patch_path) as mock_execute:
+        mock_execute().values().get().execute.return_value = {'values': [expected]}
+
+        spreadsheet_id, from_row, to_row = test_input
+        assert SheetManager.get_data_with_range(spreadsheet_id, from_row, to_row) == expected
+
+
+TEST_GET_DATA_WITH_RANGE_ERROR_DATA = [
+    (("1VgEe6d91mWE9VF54xkeJK6p", 'A1', 'A13'), None)
+]
+@pytest.mark.parametrize(
+    "test_input, expected", 
+    TEST_GET_DATA_WITH_RANGE_ERROR_DATA
+)
+def test_get_data_with_range_error(test_input, expected):
+    """
+    Test SheetManager get_data_with_range()
+    Test case when method raised googleapiclient.errors.HttpError
+
+    :param test_input: test input data (spreadsheet_id, from_row, to_row)
+    :param expected: test expected result
+    """
+    mock_patch_path = 'app.helper.sheet_manager.SheetManager.service.spreadsheets'
+    with mock.patch(mock_patch_path) as mock_execute:
+        mock_execute().values().get().execute.side_effect = googleapiclient.errors.HttpError('Test', b'Test')
+
+        spreadsheet_id, from_row, to_row = test_input
+        assert SheetManager.get_data_with_range(spreadsheet_id, from_row, to_row) == expected
+
+
+TEST_GET_ALL_DATA_TRUE_DATA = [
+    (("aWva1frw3lp"), ['test1.1', 'test1.2']),
+    (("1Vg1yuasda2"), ['test2.1', 'test2.2']),
+]
+@pytest.mark.parametrize(
+    "spreadsheet_id, expected",
+    TEST_GET_ALL_DATA_TRUE_DATA
+)
+def test_get_all_data_true(spreadsheet_id, expected):
+    """
+    Test SheetManager get_all_data()
+    Test case when method executed successfully
+
+    :param spreadsheet_id: id of spreadsheet from which data will be retrieved
+    :param expected: test expected result
+    """
+    mock_patch_path = 'app.helper.sheet_manager.SheetManager.service.spreadsheets'
+    with mock.patch(mock_patch_path) as mock_execute:
+        mock_execute().values().get().execute.return_value = {'values': [expected]}
+
+        assert SheetManager.get_all_data(spreadsheet_id) == expected
+
+
+TEST_GET_ALL_DATA_ERROR_DATA = [
+    (("aWva1frw3lsdap"), None)
+]
+@pytest.mark.parametrize(
+    "spreadsheet_id, expected",
+    TEST_GET_ALL_DATA_ERROR_DATA
+)
+def test_get_all_data_raised_error(spreadsheet_id, expected):
+    """
+    Test SheetManager get_all_data()
+    Test case when method raised googleapiclient.errors.HttpError
+
+    :param spreadsheet_id: id of spreadsheet from which data will be retrieved
+    :param expected: test expected result
+    """
+    mock_patch_path = 'app.helper.sheet_manager.SheetManager.service.spreadsheets'
+    with mock.patch(mock_patch_path) as mock_spreadsheets:
+        mock_spreadsheets().values().get().execute.side_effect = googleapiclient.errors.HttpError('Test', b'Test')
+
+        assert SheetManager.get_all_data(spreadsheet_id) == expected
+
+
+TEST_APPEND_DATA_TRUE_DATA = [
+    (("aWva1frw3lp", [10, 20]), True),
+    (("1Vg1yuasda2", ['test1.1', 'test1.2', ['radio 1', 'radio 2']]), True),
+]
+@pytest.mark.parametrize(
+    "test_input, expected", 
+    TEST_APPEND_DATA_TRUE_DATA
+)
+def test_append_data_true(test_input, expected):
+    """
+    Test SheetManager append_data()
+    Test case when method executed successfully
+
+    :param test_input: test input data (spreadsheet_id, values)
+    :param expected: test expected result
+    """
+    with mock.patch('app.helper.sheet_manager.SheetManager.service.spreadsheets') as mock_spreadsheets:
+        mock_spreadsheets().values().append().execute.return_value = None
+
+        spreadsheet_id, values = test_input
+        assert SheetManager.append_data(spreadsheet_id, values) == expected
+
+
+TEST_APPEND_DATA_VALUES_NOT_LIST_DATA = [
+    (("aWva1frw3lp", 10), None),
+    (("aWva1frw3lp", 'test data'), None)
+]
+@pytest.mark.parametrize(
+    "test_input, expected", 
+    TEST_APPEND_DATA_VALUES_NOT_LIST_DATA
+)
+def test_append_data_values_not_list(test_input, expected):
+    """
+    Test SheetManager append_data()
+    Test case when variable values isn't list   ???
+
+    :param test_input: test input data (spreadsheet_id, values)
+    :param expected: test expected result
+    """
+    spreadsheet_id, values = test_input
+    assert SheetManager.append_data(spreadsheet_id, values) == expected
+
+
+TEST_APPEND_DATA_ERROR_DATA = [
+    (("aWva1frw3lp", [1, 2, 3]), None),
+    (("1Vg1yuasda2", ['test1', 'test1.']), None),
+]
+@pytest.mark.parametrize(
+    "test_input, expected",
+    TEST_APPEND_DATA_ERROR_DATA
+)
+def test_get_all_data_error(test_input, expected):
+    """
+    Test SheetManager get_all_data()
+    Test case when method raised googleapiclient.errors.HttpError
+
+    :param test_input: test input data (spreadsheet_id, values)
+    :param expected: test expected result
+    """
+    mock_patch_path = 'app.helper.sheet_manager.SheetManager.service.spreadsheets'
+    with mock.patch(mock_patch_path) as mock_spreadsheets:
+        mock_spreadsheets().values().append().execute.side_effect = googleapiclient.errors.HttpError('Test', b'Test')
+
+        spreadsheet_id, values = test_input
+        assert SheetManager.append_data(spreadsheet_id, values) == expected
+
+
+
+TEST_GET_SHEET_ID_FROM_URL_DATA = [
+    ("https://docs.google.com/spreadsheets/d/1fKRIFn2gs", "1fKRIFn2gs"),
+    ("https://docs.google.com/spreadsheets/d/sad21d", "sad21d"),
+    ("https://docs.google.com/spreadsheets/d/u_RIF", "u_RIF")
+]
+@pytest.mark.parametrize(
+    "url, expected",
+    TEST_GET_SHEET_ID_FROM_URL_DATA,
+)
+def test_get_sheet_id_from_url(url, expected):
+    """
+    Test SheetManager get_sheet_id_from_url()
+
+    :param url: url from which will be parsed sheet_id
+    :param expected: test expected result
+    """
+    assert SheetManager.get_sheet_id_from_url(url) == expected
+
 
 
 TEST_LISTS_TO_LIST_DATA = [
@@ -16,59 +195,22 @@ TEST_LISTS_TO_LIST_DATA = [
     ([1, 2, 3, 4], [1, 2, 3, 4]),
     (None, None)
 ]
-
-TEST_GET_SHEET_ID_FROM_URL_DATA = [
-    ("https://docs.google.com/spreadsheets/d/1fKRIFn2gs", "1fKRIFn2gs"),
-    ("https://docs.google.com/spreadsheets/d/sad21d", "sad21d"),
-    ("https://docs.google.com/spreadsheets/d/u_RIF", "u_RIF"),
-    ("https://docs.google.com/spreadsheets/d/fuasdpa", "fuasdpa")
-]
-
-
-TEST_GET_DATA_WITH_RANGE = [
-    # (("1VgEe6d91mWE9VF54xkeJK6pKVKl_TjlsFPSGt3UtUx0", 'A1', 'A13'), ['Rivne', 'Lviv', 'London', 'Kyiv', 'Lutsk', 'Tokyo', 'Delhi', 'Paris', 'Chicago', 'Boston', 'Toronto', 'Madrid', 'Rome']),
-    (("1VgEe6d91mWE9VF54xkeJK6pKVKl", 'A1', 'A13'), ['Rivne', 'Lviv']),
-]
-
-
 @pytest.mark.parametrize(
-    "test_input, expected", 
+    "data, expected", 
     TEST_LISTS_TO_LIST_DATA, 
     ids=["1_2_3_5", "1_2_2", "1_2_3_4", "None"]
 )
-def test_lists_to_list(test_input, expected):
-    assert SheetManager.lists_to_list(test_input) == expected
+def test_lists_to_list(data, expected):
+    """
+    Test SheetManager get_sheet_id_from_url()
 
-@pytest.mark.parametrize(
-    "test_input, expected",
-    TEST_GET_SHEET_ID_FROM_URL_DATA,
-)
-def test_get_sheet_id_from_url(test_input, expected):
-    assert SheetManager.get_sheet_id_from_url(test_input) == expected
-
-
-@pytest.mark.parametrize("test_input, expected", TEST_GET_DATA_WITH_RANGE)
-# @mock.patch('app.helper.sheet_manager.SheetManager.service.spreadsheets.values.get.execute')
-@mock.patch('app.helper.sheet_manager.SheetManager.service.spreadsheets')
-def test_get_data_with_range(mock_execute, test_input, expected):
-    # типу що я роблю - я мокаю до першого виклику (цей рядок) а далі руками дописую (і останній (функція остання) має бути без виклику, а просто (execute) як тут)
-    mock_execute().values().get().execute.return_value = {'values': [expected]}
-    # mock_execute.return_value = {'values': [expected]}
-
-    # mock_execute.spreadsheets.return_value = \
-    #     mock_execute.spreadsheets.values.return_value = \
-    #         mock_execute.spreadsheets.values.get.return_value = \
-    #             mock_execute.spreadsheets.values.get.execute.return_value = [expected]
-
-    # import pdb; pdb.set_trace()
-
-    # mock.patch('app.helper.sheet_manager.SheetManager.service.spreadsheets().values().get().execute', mock.MagicMock(return_value=['Rivne', 'Lviv']))
-
-    spreadsheet_id, from_row, to_row = test_input
-    assert SheetManager.get_data_with_range(spreadsheet_id, from_row, to_row) == expected
+    :param data: user data
+    :param expected: test expected result
+    """
+    assert SheetManager.lists_to_list(data) == expected
 
 
-@mock.patch('app.helper.sheet_manager.SheetManager.a')
-def test_mock(mock_a):
-    mock_a.return_value = 10
-    assert SheetManager.a() == 10
+# @mock.patch('app.helper.sheet_manager.SheetManager.a')
+# def test_mock(mock_a):
+#     mock_a.return_value = 10
+#     assert SheetManager.a() == 10
