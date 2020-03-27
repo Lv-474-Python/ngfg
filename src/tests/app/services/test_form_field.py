4d65e1a595b7ce0ee2475fe3afbbf95077cf7380
AndriiKret <andrii.kret@gmail.com>
2020-03-27 19:17:36 +0200

    Refactored test for get_by_id

--- src/tests/app/services/test_form_field.py
+++ src/tests/app/services/test_form_field.py
@@ -31,26 +31,13 @@ def test_create(redis_manager_get_mock, db_mock, form_field_data):
     assert instance.position == test_instance.position
 
 
-@mock.patch('app.helper.redis_manager.RedisManager.set')
 @mock.patch('app.models.FormField.query')
 @mock.patch('app.helper.redis_manager.RedisManager.get')
-def test_get_by_id_redis_is_none(redis_manager_get_mock, query_mock, redis_manager_set_mock, form_field_data):
+def test_get_by_id(redis_manager_get_mock, query_mock, form_field_data):
     instance = FormField(**form_field_data)
 
     redis_manager_get_mock.return_value = None
     query_mock.get.return_value = instance
-    redis_manager_set_mock.return_value = None
-
-    test_instance = FormFieldService.get_by_id(1)
-
-    assert instance == test_instance
-
-
-@mock.patch('app.helper.redis_manager.RedisManager.get')
-def test_get_by_id_redis_is_not_none(redis_manager_get_mock, form_field_data):
-    instance = FormField(**form_field_data)
-
-    redis_manager_get_mock.return_value = instance
 
     test_instance = FormFieldService.get_by_id(1)
 
