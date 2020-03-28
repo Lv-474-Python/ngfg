9053131d0780c527efc60361a4f9cbd905c9f386
Yurii <yurdosii.ksv@gmail.com>
2020-03-28 13:43:01 +0200

    Added services test data

--- src/tests/app/services/test_field.py
+++ src/tests/app/services/test_field.py
@@ -9,12 +9,37 @@ from app.models import Field, Range, FieldRange, ChoiceOption, SettingAutocomple
 from app.services import FieldService
 from app.helper.errors import FieldNotExist, SettingAutocompleteNotExist
 
+from services_test_data import (
+    FIELD_SERVICE_CREATE_TEXT_OR_NUMBER_FIELD_WITHOUT_RANGE_DATA,
+    FIELD_SERVICE_CREATE_TEXT_OR_NUMBER_FIELD_WITH_RANGE_DATA,
+    FIELD_SERVICE_CREATE_TEXT_OR_NUMBER_FIELD_ERROR_DATA,
+    FIELD_SERVICE_CREATE_TEXT_AREA_TRUE_DATA,
+    FIELD_SERVICE_CREATE_TEXT_AREA_ERROR_DATA,
+    FIELD_SERVICE_CREATE_RADIO_FIELD_TRUE_DATA,
+    FIELD_SERVICE_CREATE_RADIO_FIELD_NOT_OPTIONS_DATA,
+    FIELD_SERVICE_CREATE_RADIO_FIELD_NOT_FIELD_DATA,
+    FIELD_SERVICE_CREATE_CHECKBOX_FIELD_WITHOUT_RANGE_DATA,
+    FIELD_SERVICE_CREATE_CHECKBOX_FIELD_WITH_RANGE_DATA,
+    FIELD_SERVICE_CREATE_CHECKBOX_FIELD_ERROR_DATA,
+    FIELD_SERVICE_CREATE_AUTOCOMPLETE_FIELD_TRUE_DATA,
+    FIELD_SERVICE_CREATE_AUTOCOMPLETE_FIELD_NOT_FIELD_DATA,
+    FIELD_SERVICE_CREATE_AUTOCOMPLETE_FIELD_NOT_SETTINGS_DATA,
+    FIELD_SERVICE_GET_TEXT_OR_NUMBER_ADDITIONAL_OPTIONS_TRUE_DATA,
+    FIELD_SERVICE_GET_TEXT_OR_NUMBER_ADDITIONAL_OPTIONS_EMPTY_DATA,
+    FIELD_SERVICE_GET_CHOICE_ADDITIONAL_OPTIONS_TRUE_DATA,
+    FIELD_SERVICE_GET_CHOICE_ADDITIONAL_OPTIONS_ERROR_DATA,
+    FIELD_SERVICE_GET_AUTOCOMPLETE_ADDITIONAL_OPTIONS_TRUE_DATA,
+    FIELD_SERVICE_GET_AUTOCOMPLETE_ADDITIONAL_OPTIONS_ERROR_DATA,
+    FIELD_SERVICE_GET_ADDITIONAL_OPTIONS_NUMBER_TEXT_DATA,
+    FIELD_SERVICE_GET_ADDITIONAL_OPTIONS_TEXT_AREA_DATA,
+    FIELD_SERVICE_GET_ADDITIONAL_OPTIONS_RADIO_CHECKBOX_DATA,
+    FIELD_SERVICE_GET_ADDITIONAL_OPTIONS_AUTOCOMPLETE_DATA,
+    FIELD_SERVICE_GET_ADDITIONAL_OPTIONS_ERROR_DATA,
+    FIELD_SERVICE_CHECK_FOR_RANGE_DATA
+)
+
 
 # create_text_or_number_field
-FIELD_SERVICE_CREATE_TEXT_OR_NUMBER_FIELD_WITHOUT_RANGE_DATA = [
-    ("age", 1, 1, False),
-    ("towns", 2, 1, True)
-]
 @pytest.mark.parametrize(
     "test_input",
     FIELD_SERVICE_CREATE_TEXT_OR_NUMBER_FIELD_WITHOUT_RANGE_DATA
@@ -51,10 +76,6 @@ def test_create_text_or_number_field_without_range(
     # assert result['created'] == field.created
 
 
-FIELD_SERVICE_CREATE_TEXT_OR_NUMBER_FIELD_WITH_RANGE_DATA = [
-    ("name", 3, 1, False, 2, None),
-    ("city", 4, 2, True, 2, 5)
-]
 @pytest.mark.parametrize(
     "test_input",
     FIELD_SERVICE_CREATE_TEXT_OR_NUMBER_FIELD_WITH_RANGE_DATA
@@ -106,10 +127,6 @@ def test_create_text_or_number_field_with_range(
     assert result['range']['max'] == range_max 
 
 
-FIELD_SERVICE_CREATE_TEXT_OR_NUMBER_FIELD_ERROR_DATA = [
-    ("school", 5, 2),
-    ("university", 6, 2)
-]
 @pytest.mark.parametrize(
     "test_input",
     FIELD_SERVICE_CREATE_TEXT_OR_NUMBER_FIELD_ERROR_DATA
@@ -129,10 +146,6 @@ def test_create_text_or_number_field_error(mock_field_create, test_input):
 
 
 # create_text_area
-FIELD_SERVICE_CREATE_TEXT_AREA_TRUE_DATA = [
-    ("age", 7, 3),
-    ("towns", 8, 3)
-]
 @pytest.mark.parametrize(
     "test_input",
     FIELD_SERVICE_CREATE_TEXT_AREA_TRUE_DATA
@@ -160,10 +173,6 @@ def test_create_text_area_true(mock_field_create, test_input):
     assert result['created'] == field.created
 
 
-FIELD_SERVICE_CREATE_TEXT_AREA_ERROR_DATA = [
-    ("first school", 9, 3),
-    ("first university", 10, 3)
-]
 @pytest.mark.parametrize(
     "test_input",
     FIELD_SERVICE_CREATE_TEXT_AREA_ERROR_DATA
@@ -183,10 +192,6 @@ def test_create_text_area_error(mock_field_create, test_input):
 
 
 # create_radio_field
-FIELD_SERVICE_CREATE_RADIO_FIELD_TRUE_DATA = [
-    ("age", 11, 4, ["10", "11", "12"], True),
-    ("towns", 12, 4, ["Rivne", "Lviv"], False)
-]
 @pytest.mark.parametrize(
     "test_input",
     FIELD_SERVICE_CREATE_RADIO_FIELD_TRUE_DATA
@@ -226,10 +231,6 @@ def test_create_radio_field_true(
     assert result['choiceOptions'] == choice_options
 
 
-FIELD_SERVICE_CREATE_RADIO_FIELD_NOT_OPTIONS_DATA = [
-    ("words", 13, 4, []),
-    ("text", 14, 4, None)
-]
 @pytest.mark.parametrize(
     "test_input",
     FIELD_SERVICE_CREATE_RADIO_FIELD_NOT_OPTIONS_DATA
@@ -247,10 +248,6 @@ def test_create_radio_field_not_options(test_input):
     assert result is None
 
 
-FIELD_SERVICE_CREATE_RADIO_FIELD_NOT_FIELD_DATA = [
-    ("colors", 15, 4, ["r", "g", "b"]),
-    ("cmyk", 16, 4, ["c", "m", "y", "k"])
-]
 @pytest.mark.parametrize(
     "test_input",
     FIELD_SERVICE_CREATE_RADIO_FIELD_NOT_FIELD_DATA
@@ -276,10 +273,6 @@ def test_create_radio_field_not_field(mock_field_create, test_input):
 
 
 # create_checkbox_field
-FIELD_SERVICE_CREATE_CHECKBOX_FIELD_WITHOUT_RANGE_DATA = [
-    ("age", 17, 6, ["10", "11", "12"], True),
-    ("cities", 18, 6, ["Kyiv", "Lviv"], False)
-]
 @pytest.mark.parametrize(
     "test_input",
     FIELD_SERVICE_CREATE_CHECKBOX_FIELD_WITHOUT_RANGE_DATA
@@ -319,10 +312,6 @@ def test_create_checkbox_field_without_range(
     assert result['choiceOptions'] == choice_options
 
 
-FIELD_SERVICE_CREATE_CHECKBOX_FIELD_WITH_RANGE_DATA = [
-    ("age", 19, 6, ["10", "11", "12"], True, 1, 2),
-    ("clowns", 20, 6, ["Kyiv", "Lviv"], False, 1, None)
-]
 @pytest.mark.parametrize(
     "test_input",
     FIELD_SERVICE_CREATE_CHECKBOX_FIELD_WITH_RANGE_DATA
@@ -377,10 +366,6 @@ def test_create_checkbox_field_with_range(
     assert result['range']['max'] == range_max
 
 
-FIELD_SERVICE_CREATE_CHECKBOX_FIELD_ERROR_DATA = [
-    ("beasts", 21, 5, []),
-    ("elks", 22, 5, [])
-]
 @pytest.mark.parametrize(
     "test_input",
     FIELD_SERVICE_CREATE_CHECKBOX_FIELD_ERROR_DATA
@@ -398,10 +383,6 @@ def test_create_checkbox_field_error(test_input):
 
 
 # create_autocomplete_field
-FIELD_SERVICE_CREATE_AUTOCOMPLETE_FIELD_TRUE_DATA = [
-    ("age", 23, 5, "data_url 1", "sheet 1", "A1", "A11"),
-    ("towns", 24, 5, "data_url 2", "sheet 2", "B12", "B15"),
-]
 @pytest.mark.parametrize(
     "test_input",
     FIELD_SERVICE_CREATE_AUTOCOMPLETE_FIELD_TRUE_DATA
@@ -456,10 +437,6 @@ def test_create_autocomplete_field_true(
     # якщо тут вивести field, result, settings (то settings не виведе бо в неї field None і ми типу з None беремо id в __repr__)
 
 
-FIELD_SERVICE_CREATE_AUTOCOMPLETE_FIELD_NOT_FIELD_DATA = [
-    ("crowns", 25, 5, "data_url 3", "sheet 3", "C1", "C11"),
-    ("towns", 26, 5, "data_url 4", "sheet 4", "D12", "D15")
-]
 @pytest.mark.parametrize(
     "test_input",
     FIELD_SERVICE_CREATE_AUTOCOMPLETE_FIELD_NOT_FIELD_DATA
@@ -490,10 +467,6 @@ def test_create_autocomplete_field_not_field(mock_field_create, test_input):
     # швидше всього FieldAlreadyExist бо до того це було
 
 
-FIELD_SERVICE_CREATE_AUTOCOMPLETE_FIELD_NOT_SETTINGS_DATA = [
-    ("clowns", 27, 5, "data_url 5", "sheet 5", "K1", "K11"),
-    ("towns", 28, 5, "data_url 6", "sheet 6", "F12", "F15")
-]
 @pytest.mark.parametrize(
     "test_input",
     FIELD_SERVICE_CREATE_AUTOCOMPLETE_FIELD_NOT_SETTINGS_DATA
@@ -536,10 +509,6 @@ def test_create_autocomplete_field_not_settings(
 
 
 # _get_text_or_number_additional_options
-FIELD_SERVICE_GET_TEXT_OR_NUMBER_ADDITIONAL_OPTIONS_TRUE_DATA = [
-    ("age", 29, 1, True, 1, 2),
-    ("name", 30, 1, True, 1, None)
-]
 @pytest.mark.parametrize(
     "test_input",
     FIELD_SERVICE_GET_TEXT_OR_NUMBER_ADDITIONAL_OPTIONS_TRUE_DATA
@@ -583,10 +552,6 @@ def test_get_text_or_number_additional_options_true(
     assert result['range']['max'] == range_.max
 
 
-FIELD_SERVICE_GET_TEXT_OR_NUMBER_ADDITIONAL_OPTIONS_EMPTY_DATA = [
-    (("surname", 31, 2), {}),
-    (("last name", 32, 2), {})
-]
 @pytest.mark.parametrize(
     "test_input, expected",
     FIELD_SERVICE_GET_TEXT_OR_NUMBER_ADDITIONAL_OPTIONS_EMPTY_DATA
@@ -618,10 +583,6 @@ def test_get_text_or_number_additional_options_empty(
 
 
 # _get_choice_additional_options
-FIELD_SERVICE_GET_CHOICE_ADDITIONAL_OPTIONS_TRUE_DATA = [
-    (1, ["1", "2", "3"], 2, 3),
-    (1, ["4", "4", "5"], 2, None)
-]
 @pytest.mark.parametrize(
     "test_input",
     FIELD_SERVICE_GET_CHOICE_ADDITIONAL_OPTIONS_TRUE_DATA
@@ -663,8 +624,6 @@ def test_get_choice_additional_options_true(
     assert range_.max == result['range']['max']
 
 
-FIELD_SERVICE_GET_CHOICE_ADDITIONAL_OPTIONS_ERROR_DATA = [1, 2]
-
 @pytest.mark.parametrize(
     "field_id",
     FIELD_SERVICE_GET_CHOICE_ADDITIONAL_OPTIONS_ERROR_DATA
@@ -687,10 +646,6 @@ def test_get_choice_additional_options_error(
 
 
 # _get_autocomplete_additional_options
-FIELD_SERVICE_GET_AUTOCOMPLETE_ADDITIONAL_OPTIONS_TRUE_DATA = [
-    (1, "http://docs.google.com/spreadsheet/d/asda", "sheet 1", "A1", "A2", ["1.1", "1.2"]),
-    (2, "http://docs.google.com/spreadsheet/d/wsrs", "sheet 2", "B1", "B2", ["2.1", "2.2"])
-]
 @pytest.mark.parametrize(
     "test_input",
     FIELD_SERVICE_GET_AUTOCOMPLETE_ADDITIONAL_OPTIONS_TRUE_DATA
@@ -727,8 +682,6 @@ def test_get_autocomplete_additional_options_true(
     assert result['values'] == sheet_data
 
 
-FIELD_SERVICE_GET_AUTOCOMPLETE_ADDITIONAL_OPTIONS_ERROR_DATA = [1, 2]
-
 @pytest.mark.parametrize(
     "field_id",
     FIELD_SERVICE_GET_AUTOCOMPLETE_ADDITIONAL_OPTIONS_ERROR_DATA
@@ -746,10 +699,6 @@ def test_get_autocomplete_additional_options_error(mock_settings_get, field_id):
 
 
 # get_additional_options
-FIELD_SERVICE_GET_ADDITIONAL_OPTIONS_NUMBER_TEXT_DATA = [
-    (1, 1, {'isStrict': True, 'range': {'min': 5, 'max': 10}}),
-    (1, 2, {'isStrict': False, 'range': {'min': 1, 'max': 10}}),
-]
 @pytest.mark.parametrize(
     "field_id, field_type, expected",
     FIELD_SERVICE_GET_ADDITIONAL_OPTIONS_NUMBER_TEXT_DATA
@@ -770,10 +719,6 @@ def test_get_additional_options_number_text(
     assert result == expected
 
 
-FIELD_SERVICE_GET_ADDITIONAL_OPTIONS_TEXT_AREA_DATA = [
-    (4, 3, {}),
-    (5, 3, {}),
-]
 @pytest.mark.parametrize(
     "field_id, field_type, expected",
     FIELD_SERVICE_GET_ADDITIONAL_OPTIONS_TEXT_AREA_DATA
@@ -788,10 +733,6 @@ def test_get_additional_options_text_area(field_id, field_type, expected):
     assert result == expected
 
 
-FIELD_SERVICE_GET_ADDITIONAL_OPTIONS_RADIO_CHECKBOX_DATA = [
-    (6, 4, {'choiceOptions': ['Yes', 'No']}),
-    (7, 6, {'choiceOptions': ['Male', 'Female', 'Other'], 'range': {'min': 1, 'max': 2}}),
-]
 @pytest.mark.parametrize(
     "field_id, field_type, expected",
     FIELD_SERVICE_GET_ADDITIONAL_OPTIONS_RADIO_CHECKBOX_DATA
@@ -813,26 +754,6 @@ def test_get_additional_options_radio_checkbox(
     assert result == expected
 
 
-FIELD_SERVICE_GET_ADDITIONAL_OPTIONS_AUTOCOMPLETE_DATA = [
-    (8, 5, {
-        'settingAutocomplete': {
-            'dataUrl': 'http://docs.google.com/spreadsheet/d/a', 
-            'sheet': 'sheet',
-            'fromRow': 'A1',
-            'toRow': 'A2'
-        },
-        'values': ['male', 'female']
-    }),
-    (9, 5, {
-        'settingAutocomplete': {
-            'dataUrl': 'http://docs.google.com/spreadsheet/d/aasd', 
-            'sheet': 'sheet 2',
-            'fromRow': 'B1',
-            'toRow': 'C2'
-        },
-        'values': ['yes', 'no']
-    } ),
-]
 @pytest.mark.parametrize(
     "field_id, field_type, expected",
     FIELD_SERVICE_GET_ADDITIONAL_OPTIONS_AUTOCOMPLETE_DATA
@@ -854,10 +775,6 @@ def test_get_additional_options_autocomplete(
     assert result == expected
 
 
-FIELD_SERVICE_GET_ADDITIONAL_OPTIONS_ERROR_DATA = [
-    (9, 4),
-    (10, 6),
-]
 @pytest.mark.parametrize(
     "field_id, field_type",
     FIELD_SERVICE_GET_ADDITIONAL_OPTIONS_ERROR_DATA
@@ -879,10 +796,6 @@ def test_get_additional_options_error(
 
 
 # check_for_range
-FIELD_SERVICE_CHECK_FOR_RANGE_DATA = [
-    ({'range': {'min': 2, 'max': 4}}, (2, 4)),
-    ({}, (None, None))
-]
 @pytest.mark.parametrize(
     "data, expected",
     FIELD_SERVICE_CHECK_FOR_RANGE_DATA
