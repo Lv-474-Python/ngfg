e5c843457a14b31e2d8f147c26898b18bb005490
Mykola Matviiv <matviivcareer@gmail.com>
2020-03-28 20:52:21 +0200

    enough coverage

--- src/tests/app/services/test_group.py
+++ src/tests/app/services/test_group.py
@@ -3,6 +3,25 @@ import mock
 
 from app.services import GroupService
 from app.models import Group, GroupUser
+from app.helper.errors import GroupNotExist
+
+
+@pytest.fixture()
+def group_data():
+    data = {'id': 1, 'name': 'string', 'owner_id': 1}
+    return data
+
+
+@pytest.fixture()
+def group_user_data():
+    user = {'user_id': 1, 'group_id': 1}
+    return user
+
+
+@pytest.fixture()
+def user_data():
+    user = {'id': 1, "username": "string", "email": "testmail1@gmail.com", "is_active": True}
+    return user
 
 
 @pytest.fixture()
@@ -13,10 +32,12 @@ def group_data():
     }
     return data
 
+
 @pytest.fixture()
 def group_user_data():
     user = {'user_id': 1, 'group_id': 1}
     return user
+
 
 @pytest.fixture()
 def group_data_with_id():
@@ -24,31 +45,6 @@ def group_data_with_id():
         'id': 1,
         'name': 'TestGroupName1',
         'owner_id': 1
-    }
-    return data
-
-
-@pytest.fixture()
-def group_data_with_users():
-    data = {
-        'id': 1,
-        'name': 'TestGroupName1',
-        'owner_id': 1,
-        'group_users': [
-            {'isActive': False, 'id': 2, 'email': 'testmail2@gmail.com', 'username': None},
-            {'isActive': False, 'id': 3, 'email': 'testmail3@gmail.com', 'username': None},
-            {'isActive': True, 'id': 1, 'email': 'testmail1@gmail.com',
-             'username': 'test_username'},
-            {'isActive': False, 'id': 4, 'email': 'testmail4@gmail.com', 'username': None}
-        ]
-    }
-    return data
-
-
-@pytest.fixture()
-def group_users():
-    data = {
-
     }
     return data
 
@@ -194,65 +190,6 @@ def test_update_group_owner_id(db_mock, group_service_mock, group_data_with_id):
     assert updated_instance.owner_id == update_result.owner_id
 
 
-# TODO mock relation group.group_users and finish this test
-#
-# @mock.patch('app.services.UserService.to_json')
-# @mock.patch('app.services.group.GroupService.get_by_id')
-# def test_get_users_by_group(group_service, user_service, group_data):
-#
-#     user1_data = {'username': 'TestUser1', 'email': 'testmail1@gmail.com',
-#                   'google_token': '118123199453900245179', 'is_active': 'True'}
-#     user2_data = {'username': 'TestUser2', 'email': 'testmail2@gmail.com',
-#                   'google_token': '228223299453900245279', 'is_active': 'True'}
-#     user3_data = {'username': 'TestUser3', 'email': 'testmail3@gmail.com',
-#                   'google_token': '338333399453900345379', 'is_active': 'True'}
-#
-#     user1 = User(**user1_data)
-#     user2 = User(**user2_data)
-#     user3 = User(**user3_data)
-#
-#     users = [user1, user2, user3]
-#
-#     users_json = [
-#         {'isActive': True, 'id': 2, 'email': 'testmail2@gmail.com', 'username': 'TestUser2'},
-#         {'isActive': True, 'id': 3, 'email': 'testmail3@gmail.com', 'username': 'TestUser3'},
-#         {'isActive': True, 'id': 1, 'email': 'testmail1@gmail.com', 'username': 'TestUser1'},
-#     ]
-#
-#     group_instance = Group(**group_data)
-#     group_instance.groups_users = users
-#     group_service.return_value = group_instance
-#     user_service.return_value = users_json
-#
-#     test_users = GroupService.get_users_by_group(1)
-#
-#     assert users == test_users
-
-import pytest
-import mock
-
-from app.services import GroupService
-from app.models import Group, GroupUser
-
-
-@pytest.fixture()
-def group_data():
-    data = {'id': 1, 'name': 'string', 'owner_id': 1}
-    return data
-
-
-@pytest.fixture()
-def group_user_data():
-    user = {'user_id': 1, 'group_id': 1}
-    return user
-
-
-@pytest.fixture()
-def user_data():
-    user = {'id': 1, "username": "string", "email": "jojo@g.mail", "is_active": False}
-    return user
-
-
 @mock.patch('app.services.UserService.to_json')
 @mock.patch('app.services.GroupService.get_by_id')
 def test_get_users_by_group(get_by_id_mock, to_json_mock, group_data, group_user_data, user_data):
@@ -267,3 +204,11 @@ def test_get_users_by_group(get_by_id_mock, to_json_mock, group_data, group_user
 
     assert [user_json, user_json] == test_instance
 
+
+@mock.patch('app.services.GroupService.get_by_id')
+def test_get_users_by_group_error(get_by_id_mock):
+    # idk how to write this with decorator
+    # pytest.mock.xfail dont pass
+    with pytest.raises(GroupNotExist):
+        get_by_id_mock.return_value = None
+        GroupService.get_users_by_group(1)
