a0a8904491d53c453a0708dfd0965d2c782a10f6
AndriiKret <andrii.kret@gmail.com>
2020-03-28 21:47:33 +0200

    Changed redis test

--- src/tests/app/helper/test_redis_manager.py
+++ src/tests/app/helper/test_redis_manager.py
@@ -5,5 +5,5 @@ from app.helper.redis_manager import RedisManager
 @mock.patch('app.REDIS')
 def test_delete(redis_mock):
     redis_mock.return_value = 0
-    redis_delete = RedisManager.delete('jojo')
+    redis_delete = RedisManager.delete('form_fields:')
     assert redis_delete == 0
