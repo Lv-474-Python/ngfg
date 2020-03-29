026da36cfa0a3b14753d4f7b6d192d4ef7da2a8a
Teemmer <dziga2000@gmail.com>
2020-03-29 15:32:51 +0300

    Revorked

--- src/tests/app/services/test_field.py
+++ src/tests/app/services/test_field.py
@@ -98,119 +98,136 @@ def test_update_text_or_number_field_delete_range_not_exist(
     assert updated is None
 
 
-# @mock.patch('app.services.FieldService.update')
-# @mock.patch('app.services.FieldService.delete')
-# @mock.patch('app.services.FieldRangeService.get_by_field_id')
-# def test_update_text_or_number_field_delete_range_not_deleted(
-#         field_range_service_mock,
-#         field_service_mock_delete,
-#         field_service_mock_update,
-#         text_or_number_update_data,
-#         range_data
-# ):
-#     """
-#     Test update_text_or_number delete range, when range not deleted from database
-#     Raise FieldRangeNotDeleted()
-#     """
-#     field_service_mock_update.return_value = text_or_number_update_data
-#     field_service_mock_delete.return_value = None
-#     delete_range = True
-#     field_range_service_mock.return_value = range_data
-#
-#     updated = FieldService.update_text_or_number_field(  # pylint: disable=too-many-arguments
-#         field_id=text_or_number_update_data.get('id'),
-#         name=text_or_number_update_data.get('name'),
-#         range_min=text_or_number_update_data.get('range_min'),
-#         range_max=text_or_number_update_data.get('range_max'),
-#         delete_range=delete_range,
-#         is_strict=text_or_number_update_data.get('is_strict'))
-#
-#     assert updated is None
-#
-#
-# @mock.patch('app.schemas.FieldNumberTextSchema')
-# @mock.patch('app.services.RangeService')
-# @mock.patch('app.services.FieldRangeService')
-# @mock.patch('app.services.FieldService.update')
-# def test_update_text_or_number_field_update_range(
-#         field_service_mock,
-#         field_range_service_mock,
-#         range_service_mock,
-#         schema_mock,
-#         text_or_number_update_data,
-#         field_data,
-#         range_data
-# ):
-#     """
-#     Test update_text_or_number, update range
-#     """
-#     field_service_mock.return_value = Field(**field_data)
-#     schema_mock.dump.return_value = field_data
-#
-#     field_range_service_mock.get_by_field_id.return_value = range_data
-#     range_service_mock.create.return_value = None
-#     field_range_service_mock.update.return_value = None
-#
-#     delete_range = False
-#     updated = FieldService.update_text_or_number_field(  # pylint: disable=too-many-arguments
-#         field_id=text_or_number_update_data.get('id'),
-#         name=text_or_number_update_data.get('name'),
-#         range_min=text_or_number_update_data.get('range_min'),
-#         range_max=text_or_number_update_data.get('range_max'),
-#         delete_range=delete_range,
-#         is_strict=text_or_number_update_data.get('is_strict'))
-#
-#     assert text_or_number_update_data['name'] == updated['name']
-#     assert text_or_number_update_data['id'] == updated['id']
-#     assert text_or_number_update_data['range_min'] == updated['range']['min']
-#     assert text_or_number_update_data['range_max'] == updated['range']['max']
-#
-#
-# @mock.patch('app.services.FieldService.get_additional_options')
-# @mock.patch('app.services.FieldRangeService.create')
-# @mock.patch('app.services.RangeService.create')
-# @mock.patch('app.services.FieldRangeService.get_by_field_id')
-# @mock.patch('app.services.FieldService.update')
-# @mock.patch('app.schemas.FieldNumberTextSchema.dump')
-# def test_update_text_or_number_create_range(
-#         schema_mock,
-#         field_service_update,
-#         field_range_get,
-#         range_create,
-#         field_range_create,
-#         get_add_options,
-#         field_data,
-#         range_data,
-#         field_range_data
-# ):
-#     """
-#     Test text or number update, create range
-#     """
-#     field = Field(**field_data)
-#     f_range = Range(**range_data)
-#     field_range = FieldRange(**field_range_data)
-#
-#     field_service_update.return_value = field
-#     schema_mock.return_value = field_data
-#     field_range_get.return_value = None
-#     range_create.return_value = f_range
-#     field_range_create.return_value = field_range
-#     get_add_options.return_value = {'range': range_data}
-#
-#     delete_range = False
-#
-#     updated = FieldService.update_text_or_number_field(
-#             field_id=field.id,
-#             name=field.name,
-#             range_max=f_range.max,
-#             range_min=f_range.min,
-#             delete_range=delete_range,
-#             is_strict=field.is_strict
-#     )
-#     print(updated)
-#     assert updated['name'] == field.name
-#     assert updated['range']['min'] == f_range.min
-#     assert updated['range']['max'] == f_range.max
+@mock.patch('app.services.FieldRangeService.delete')
+@mock.patch('app.services.FieldRangeService.get_by_field_id')
+@mock.patch('app.services.FieldService.update')
+@mock.patch('app.schemas.FieldNumberTextSchema.dump')
+def test_update_text_or_number_delete_range_not_deleted(
+        schema_mock,
+        field_service_update,
+        field_range_get,
+        field_range_delete,
+        field_data,
+        field_range_data
+):
+    """
+    Test update_text_or_number delete range, when range not deleted from database
+    Raise FieldRangeNotDeleted()
+    """
+    field = Field(**field_data)
+    field_range = FieldRange(**field_range_data)
+
+    field_service_update.return_value = field
+    schema_mock.return_value = field_data
+    field_range_get.return_value = field_range
+
+    delete_range = True
+    field_range_delete.return_value = None
+
+    updated = FieldService.update_text_or_number_field(
+            field_id=field.id,
+            name=field.name,
+            range_max=None,
+            range_min=None,
+            delete_range=delete_range,
+            is_strict=field.is_strict
+    )
+
+    assert updated is None
+
+
+@mock.patch('app.services.FieldService.get_additional_options')
+@mock.patch('app.services.FieldRangeService.update')
+@mock.patch('app.services.RangeService.create')
+@mock.patch('app.services.FieldRangeService.get_by_field_id')
+@mock.patch('app.services.FieldService.update')
+@mock.patch('app.schemas.FieldNumberTextSchema.dump')
+def test_update_text_or_number_update_range(
+        schema_mock,
+        field_service_update,
+        field_range_get,
+        range_create,
+        field_range_update,
+        get_add_options,
+        field_data,
+        range_data,
+        field_range_data
+):
+    """
+    Test text or number update, update range
+    """
+    field = Field(**field_data)
+    f_range = Range(**range_data)
+    field_range = FieldRange(**field_range_data)
+
+    field_service_update.return_value = field
+    schema_mock.return_value = field_data
+    field_range_get.return_value = field_range
+    range_create.return_value = f_range
+    field_range_update.return_value = field_range
+    get_add_options.return_value = {'range': range_data}
+
+    delete_range = False
+
+    updated = FieldService.update_text_or_number_field(
+            field_id=field.id,
+            name=field.name,
+            range_max=f_range.max,
+            range_min=f_range.min,
+            delete_range=delete_range,
+            is_strict=field.is_strict
+    )
+
+    assert updated['name'] == field.name
+    assert updated['range']['min'] == f_range.min
+    assert updated['range']['max'] == f_range.max
+
+
+@mock.patch('app.services.FieldService.get_additional_options')
+@mock.patch('app.services.FieldRangeService.create')
+@mock.patch('app.services.RangeService.create')
+@mock.patch('app.services.FieldRangeService.get_by_field_id')
+@mock.patch('app.services.FieldService.update')
+@mock.patch('app.schemas.FieldNumberTextSchema.dump')
+def test_update_text_or_number_create_range(
+        schema_mock,
+        field_service_update,
+        field_range_get,
+        range_create,
+        field_range_create,
+        get_add_options,
+        field_data,
+        range_data,
+        field_range_data
+):
+    """
+    Test text or number update, create range
+    """
+    field = Field(**field_data)
+    f_range = Range(**range_data)
+    field_range = FieldRange(**field_range_data)
+
+    field_service_update.return_value = field
+    schema_mock.return_value = field_data
+    field_range_get.return_value = None
+    range_create.return_value = f_range
+    field_range_create.return_value = field_range
+    get_add_options.return_value = {'range': range_data}
+
+    delete_range = False
+
+    updated = FieldService.update_text_or_number_field(
+            field_id=field.id,
+            name=field.name,
+            range_max=f_range.max,
+            range_min=f_range.min,
+            delete_range=delete_range,
+            is_strict=field.is_strict
+    )
+
+    assert updated['name'] == field.name
+    assert updated['range']['min'] == f_range.min
+    assert updated['range']['max'] == f_range.max
 
 
 @mock.patch('app.schemas.FieldSettingAutocompleteSchema.dump')
