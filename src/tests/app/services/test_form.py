316b3a922aa9dff9be32f821df41445c4f9ac0b8
AndriiKret <59411736+AndriiKret@users.noreply.github.com>
2020-03-30 15:11:36 +0300

    Added FieldForm CRUD tests and FormService.to_json test(#218) (#225)
    * Added test for FieldForm create, get_by_id
    
    * Refactored test for get_by_id
    
    * Added test FieldForm update (without raising error coverage)
    
    * updated req
    
    * Added DB secrets
    
    * Added update and filter test with different params
    
    * Added redis secret
    
    * Added delete tests
    
    * Changed redis test
    
    * Added Form to_json test
    
    * CI test 1
    
    * CI test 2
    
    * CI test 3
    
    * CI test 4
    
    * CI test 5
    
    * Formatting

--- src/tests/app/services/test_form.py
+++ src/tests/app/services/test_form.py
@@ -7,6 +7,7 @@ import pytest
 
 from app.models import Form
 from app.services import FormService
+from app.schemas import FormSchema
 
 from .services_test_data import (
     FORM_SERVICE_CREATE_DATA,
@@ -109,6 +110,7 @@ def form():
         is_published=True
     )
     return form
+
 
 @pytest.mark.parametrize(
     "form_id, owner_id, name, title, result_url, is_published",
@@ -355,3 +357,25 @@ def test_filter_by_all(
     )
 
     assert result == [form]
+
+
+@pytest.fixture()
+def form_before_dump_data():
+    data = {"id": 1, "owner_id": 1, "name": 'string', "title": 'string', "result_url": 'url@mail.com',
+            "is_published": True, "created": 'date'}
+    return data
+
+
+@pytest.fixture()
+def form_after_dump_data():
+    data = {"id": 1, "ownerId": 1, "name": 'string', "title": 'string', "resultUrl": 'url@mail.com',
+            "isPublished": True, "created": 'date'}
+    return data
+
+
+@mock.patch('app.schemas.FormSchema')
+def test_to_json(schema_mock, form_before_dump_data, form_after_dump_data):
+    schema_mock.return_value = FormSchema(many=False)
+
+    test_instance = FormService.to_json(form_before_dump_data)
+    assert test_instance == form_after_dump_data
