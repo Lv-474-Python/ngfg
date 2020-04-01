bedce04d6504c6260760ecfc4ed5f4d01c746b29
Yurii <yurdosii.ksv@gmail.com>
2020-04-01 18:26:59 +0300

    Updated ShareFormService and GroupService

--- src/app/services/shared_form.py
+++ src/app/services/shared_form.py
@@ -19,7 +19,7 @@ class SharedFormService:
         return (not bool(errors), errors)
 
     @staticmethod
-    def convertStringToDateTime(datetime_string):
+    def convert_string_to_datetime(datetime_string):
         """
         Convert datetime_string to datetime instance
 
@@ -37,3 +37,25 @@ class SharedFormService:
 
         datetime_instance = datetime.fromisoformat(datetime_string)
         return datetime_instance
+
+    @staticmethod
+    def get_token_initial_payload(form_id, exp=None, nbf=None):
+        """
+        Get token initial payload
+
+        :param form_id: id of form that will be shared
+        """
+        payload = {
+            'form_id': form_id,
+            'group_id': None,
+        }
+
+        if exp is not None:
+            exp = SharedFormService.convert_string_to_datetime(exp)
+            payload['exp'] = exp
+
+        if nbf is not None:
+            nbf = SharedFormService.convert_string_to_datetime(nbf)
+            payload['nbf'] = nbf
+
+        return payload
