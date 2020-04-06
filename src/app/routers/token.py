4dfaf6f3ed7f8884b676e65867c15686070be003
Yurii <yurdosii.ksv@gmail.com>
2020-04-06 14:16:38 +0300

    Updated token route

--- src/app/routers/token.py
+++ src/app/routers/token.py
@@ -5,11 +5,10 @@ Token resource API
 from werkzeug.exceptions import BadRequest
 from flask import request, jsonify, Response
 from flask_restx import Resource, fields
-from flask_login import login_required, current_user
+from flask_login import current_user
 
 from app import API
 from app.services import FormService, TokenService, GroupService
-# from app.helper.jwt_helper import decode_token
 
 
 TOKEN_NS = API.namespace('tokens')
@@ -39,28 +38,27 @@ class TokenCheckAPI(Resource):
 
         :param token: token to check
         """
-        # eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJmb3JtX2lkIjo1NywiZ3JvdXBfaWQiOm51bGwsImV4cCI6MTU4NjEyMTE2MywibmJmIjoxNTg2MTEwMzYzfQ.RoG0MRA-ll5Xkzo2bj6FRUd956gRJrR06jd8BZMsQHQ
 
         # чи треба перевіряти чи токен створений нами
 
 
         token_data = TokenService.decode_token_for_check(token)
         if token_data is None:
-            raise BadRequest({'errors': 'Wrong token'}) # може 404 скліпати на фронті
+            raise BadRequest({'errors': 'Wrong token'}) # Not enough token segments
 
         is_correct, _ = TokenService.validate_data(token_data)
         if not is_correct:
-            raise BadRequest({'errors': 'Wrong token data'})
+            raise BadRequest({'errors': 'Wrong token'}) # Token isn't valid
 
         form_id = token_data.get('form_id')
         form = FormService.get_by_id(form_id)
         if form is None:
-            raise BadRequest({'errors': 'Wrong form'}) # form doesn't exist
+            raise BadRequest({'errors': 'Wrong token'}) # Form doesn't exist
 
         group_id = token_data.get('group_id')
         if group_id is not None:
             group = GroupService.get_by_id(group_id)
             if group is None:
-                raise BadRequest({'errors': 'Wrong group'}) # group doesn't exist
+                raise BadRequest({'errors': 'Wrong token'}) # Group doesn't exist
 
         return Response(status=204)
