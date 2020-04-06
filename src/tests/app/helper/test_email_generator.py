dc2c206b08dd8ef5a7218309200ba4fc5b1520bd
IvanKostiv <ivankostiv.pz@gmail.com>
2020-04-06 15:30:53 +0300

    Added test to email_generator

--- src/tests/app/helper/test_email_generator.py
+++ src/tests/app/helper/test_email_generator.py
@@ -1,14 +1,13 @@
 import pytest
 from app.helper.email_generator import generate_share_field_message
-from app.models import Field
+from flask_mail import Message
 
 import mock
 
 
-@mock.patch('flask.url_for')
-def test_generate_share_field_message(url_mock):
-    email = 'test@gmail.com'
-    field = {
+@pytest.fixture
+def field():
+    data = {
         'id': 1,
         'name': 'TestField',
         'owner_id': 2,
@@ -16,6 +15,22 @@ def test_generate_share_field_message(url_mock):
         'is_strict': False
     }
 
-    url_mock.return_value = 'link'
+    return data
 
-    print(generate_share_field_message(email, field))
+
+@mock.patch('app.helper.email_generator.url_for')
+def test_generate_share_field_message(url_mock, field, app):
+    email = 'test@gmail.com'
+    link = 'link'
+
+    url_mock.return_value = link
+
+    message = Message('Shared Field', recipients=[email])
+    message.html = (
+            f'<h3>Hello, {email}!\n' +
+            f'To add field: {field["name"]} to your collection, click this link: {link}</h3>'
+    )
+
+    result = generate_share_field_message(email, field)
+
+    assert message.html == result.html
